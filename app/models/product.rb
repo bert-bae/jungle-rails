e76553828770c5dce68581ffbe38ba360662d0fc
Elbert <elbert.bae@gmail.com>
2018-12-11 22:45:17 +0000

    delete product with reviews works

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,9 @@
 class Product < ActiveRecord::Base
-
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
   validates :price, presence: true
